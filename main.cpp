842b7b8064079b4b74dc0c9402026742f31f656b
Sephora <spierrelouis@ufl.edu>
2025-10-30 17:19:01 -0400

    Implemented Main Menu Named program "Amplify!!"

--- main.cpp
+++ main.cpp
@@ -7,14 +7,29 @@ using namespace std;
 int main() {
 
    //Load Songs
-   vector<Song> songs = loadSongs("dataset.csv");
-   cout << "Loaded " << songs.size() << " songs" << endl;
+    vector<Song> songs = loadSongs("dataset.csv");
+    cout << "Loaded " << songs.size() << " songs" << endl;
+    Heap songHeap;
 
-   Heap songHeap;
-
+    //Insert Songs into heap
     for (auto&  song: songs) {
         songHeap.insert(song);
     }
+
+    cout << "\n----------- Amplify!!!! -------" << endl;
+    cout << "Song Recommender Based on Energy " << endl;
+    cout << "\n -------------Menu--------------" << endl;
+    cout << "1. View All Songs" << endl;
+    cout << "2. Add New Song" << endl;
+    cout << "3. Delete Song" << endl;
+    cout << "4. Recommend Top N Songs by Energy Level" << endl;
+    cout << "5. Find Energy Level of a Song" <<  endl;
+    cout << "6. Find Songs in Range Query" << endl;
+    cout << "7. Compare Heap vs Red-Black Tree Performance" << endl;
+    cout << "8. Exit" << endl;
+    cout << "--------------------------------" << endl;
+    
+    
 
 }
 
